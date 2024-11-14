09a04c020db5bad6ada890d313f7eedaa3555ad7
flaviaguerreiro <guerreiro.flavia@gmail.com>
2024-11-14 10:05:54 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({'nome_Servifor': ['Adriana', 'Mônica', 'Samara'], 
+df = pd.DataFrame({'nome_Servidor': ['Adriana', 'Mônica', 'Samara'], 
                    'Salário': [1200, 300, 5000]})
 
 st.write('Crinando uma tabela!')
 st.write(df)
+
+opcao = st.selectbox('Qual servidor você gostariade selecionar?', df['nome_Servidor'])
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nome_Servidor'] == opcao]
+dadosFiltrados
