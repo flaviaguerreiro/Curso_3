eb4360504c72ecaaff870a71e9f4bde5233a2569
flaviaguerreiro <guerreiro.flavia@gmail.com>
2024-11-14 12:33:34 -0300

    Update aula9_2.py

--- aula9_2.py
+++ aula9_2.py
@@ -4,7 +4,9 @@ import pandas as pd
 st.title('LOCALIZAÇÃO DAS COMUNIDADES QUILOMBOLAS (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
+#limpando e organizando os dados
 df.drop(columns=['Unnamed: 0'], inplace=True)
+df.sort_values('CD_UF')
 
 #convertendo latitude e longitude em número
 list = ['Lat_d', 'Long_d']
@@ -13,10 +15,17 @@ df[list] =df[list].apply(pd.to_numeric, errors='coerce')
 #criando lista com cada estado aparecendo uma única vez
 estados = df['NM_UF'].unique()
 
+#inserindo caixa para seleção do estado 
 estado_filtro = st.selectbox('Selecione o Estado:', estados)
 
+#criando filtro para possibilitar a seleção dos dados de cada estado
 dados_filtrados = df[df['NM_UF'] == estado_filtro]
 
+#inserindo botão que permite exibir ou não a tabela de dados
 if st.checkbox('Mostrar Tabela'):
   st.write(dados_filtrados)
+
+#inserindo o mapa a partir da variável dados_filtrados
 st.map(dados_filtrados, latitude='Lat_d', longitude='Long_d')
+
+
