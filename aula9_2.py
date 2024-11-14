8e78c29117e21d239350e04abca2dc7a7b8300c1
flaviaguerreiro <guerreiro.flavia@gmail.com>
2024-11-14 11:30:49 -0300

    Update aula9_2.py

--- aula9_2.py
+++ aula9_2.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 st.title('LOCALIZAÇÃO DAS COMUNIDADES QUILOMBOLAS (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df.drop(columns=['Unnamed: 0'], inplace=True)
+#convertendo latitude e longitude em número
+list = ['Lat_d', 'Long_d']
+df[list] =df[list].apply(pd.to_numeric, errors='coerce')
+
+#criando lista com cada estado aparecendo uma única vez
+estados = df['NM_UF'].unique()
